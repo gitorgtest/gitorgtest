9ff6c7030033e46ffd9c82ec86d6b94c28253683
rikimarutest <jyunpei0811@hotmail.com>
2015-06-28 02:52:13 +0900

    コメント追加

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -2,6 +2,7 @@
 
 require 'core/ClassLoader.php';
 
+//classローダのインスタンス作成
 $loader = new ClassLoader();
 $loader->registerDir(dirname(__FILE__).'/core');
 $loader->registerDir(dirname(__FILE__).'/models');
